25684f02772f9a7b8517bc228e34c45f37ee6c67
siposbence <30337626+siposbence@users.noreply.github.com>
2021-11-29 23:01:35 +0100

    run_it

---
+++ run.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+import os
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("$SYS/#")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    if msg.topic == "start":
+      os.system("/home/pi/robot_szem/start.sh")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.2.73", 1883, 60)
+
+client.loop_forever()
