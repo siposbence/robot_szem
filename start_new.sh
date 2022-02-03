a73acd501225827507fcac73cd14e54b30cf5440
siposbence <30337626+siposbence@users.noreply.github.com>
2022-02-03 13:39:52 +0100

    Create start_new.sh

---
+++ start_new.sh
@@ -0,0 +1,3 @@
+chromium-browser "file:///home/pi/robot_szem/eyes_xy_left.html" --new-window --disable-gpu-vsync --kiosk --window-position=0,0  &
+chromium-browser "file:///home/pi/robot_szem/eyes_xy_right.html" --new-window --disable-gpu-vsync --kiosk --window-position=480,0 --user-data-dir=/tmp           
+#echo "futott_" > /home/pi/Desktop/cron.txt
